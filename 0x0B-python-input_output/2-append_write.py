f03c512658fd095085b7d0103d25cd5f4060e1b4
lia237 <https://github.com/lia237/alx-low_level_programming.git>
2023-09-13 06:16:26 -0700

    test

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-def read_lines(filename="", nb_lines=0):
-    """ function reads n lines of a text file (UTF8) prints it stdout """
+"""
+append_write module
+"""
 
-    with open(filename, encoding='utf-8') as f:
-        lines = f.readlines()
-        if nb_lines <= 0 or nb_lines >= len(lines):
-            [print(i, end='') for i in lines]
-        else:
-            [print(lines[i], end='') for i in range(0, nb_lines)]
+
+def append_write(filename="", text=""):
+    """
+    write_file - appends a string at the end of a text file (UTF8),
+                and returns the number of characters added:
+    Args:
+        filename: name of the file
+        text: text to be written
+    Return: number of bytes written.
+    """
+    with open(filename, mode="a", encoding="UTF-8") as f:
+        return (f.write(text))
